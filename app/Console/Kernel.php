fe3b5674d959e87f5f4c11d0083bb19113875d7b
OsamaDasooky <osamadasooky6@gmail.com>
2022-10-24 22:00:31 +0300

    prefinal

---
+++ app/Console/Kernel.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Console;
+
+use App\Models\Donation;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+
+class Kernel extends ConsoleKernel
+{
+    /**
+     * Define the application's command schedule.
+     *
+     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+     * @return void
+     */
+    protected function schedule(Schedule $schedule)
+    {
+        // $schedule->command('inspire')->hourly();
+        $schedule->call(function () {
+            DB::table('projects')->whereDate('starting_date', '<', now())->update(['status' => 'in progress']);
+        })->everyMinute();
+
+        $schedule->call(function () {
+            $donations = DB::table('projects')->join('donations', 'projects.id', '=', 'donations.project_id')->select('projects.*, donations.amount')->sum('amount');
+            DB::table('projects')->where('projects.target_donations', '<=', $donations)->update(['status' => 'in progress']);
+        })->everyMinute();
+    }
+
+    /**
+     * Register the commands for the application.
+     *
+     * @return void
+     */
+    protected function commands()
+    {
+        $this->load(__DIR__ . '/Commands');
+
+        require base_path('routes/console.php');
+    }
+}
